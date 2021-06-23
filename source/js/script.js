7e23ed20e2afb2499d789530c9fe2dd96b9e2db4
Anastasiia Ushakova <anastasiia.b.ushakova@gmail.com>
2021-06-23 21:16:35 +0700

    Шапка проекта(2-ая часть)

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,26 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+navMain.classList.remove('main-nav--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+navToggle.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (navMain.classList.contains('main-nav--opened')) {
+      evt.preventDefault();
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
   }
 });
