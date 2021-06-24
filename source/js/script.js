6eed9602aa61ffeb939ae43ee33cbe6f8275fdb1
Anastasiia Ushakova <anastasiia.b.ushakova@gmail.com>
2021-06-25 01:59:55 +0700

    Валидация формы

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,8 @@
 'use strict';
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+const form = document.querySelector('.form');
+const inputUserPhone = form.querySelector('#user-phone');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -24,3 +26,44 @@ window.addEventListener('keydown', function (evt) {
     }
   }
 });
+
+inputUserPhone.addEventListener('invalid', () => {
+  console.log(inputUserPhone.validity);
+  if (inputUserPhone.validity.valueMissing) {
+    inputUserPhone.setCustomValidity('Обязательное поле');
+    return;
+  }
+  if (inputUserPhone.validity.patternMismatch) {
+    inputUserPhone.setCustomValidity('Введите число');
+    return;
+  }
+  inputUserPhone.setCustomValidity('');
+  // inputUserPhone.reportValidity();
+});
+
+const resetForm = () => {
+  const inputUserName = form.querySelector('#user-name');
+  inputUserName.value = '';
+  inputUserPhone.value = '';
+  console.log(inputUserName.value);
+  console.log(inputUserPhone.value);
+};
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  const formData = new FormData(evt.target);
+
+  fetchData('https://echo.htmlacademy.ru/',
+    {
+      method: 'POST',
+      body: formData,
+    }, () =>
+      resetForm());
+});
+
+const fetchData = (url, options, onSuccess) => {
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((response) => onSuccess(response));
+};
