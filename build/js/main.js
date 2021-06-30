b263da209c55a583e63956942b4f9899c88b2f49
Anastasiia Ushakova <anastasiia.b.ushakova@gmail.com>
2021-07-01 01:44:15 +0700

    Файлы JS - модули в IIFE

--- build/js/main.js
+++ build/js/main.js
@@ -1,89 +1,103 @@
 'use strict';
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var anchors = document.querySelectorAll('a[href*="#"]');
 var form = document.querySelector('.form');
 var inputUserPhone = form.querySelector('#user-phone');
-var anchors = document.querySelectorAll('a[href*="#"]');
 
-navMain.classList.remove('main-nav--nojs');
+(function () {
+  navMain.classList.remove('main-nav--nojs');
+})();
 
-navToggle.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
+(function () {
+  navMain.classList.remove('main-nav--nojs');
 
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27) {
-    if (navMain.classList.contains('main-nav--opened')) {
-      evt.preventDefault();
+  navToggle.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
       navMain.classList.add('main-nav--closed');
       navMain.classList.remove('main-nav--opened');
     }
-  }
-});
+  });
+})();
 
-anchors.forEach(function (anchor) {
-  anchor.addEventListener('click', function (evt) {
+(function () {
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      if (navMain.classList.contains('main-nav--opened')) {
+        evt.preventDefault();
+        navMain.classList.add('main-nav--closed');
+        navMain.classList.remove('main-nav--opened');
+      }
+    }
+  });
+})();
+
+(function () {
+  anchors.forEach(function (anchor) {
+    anchor.addEventListener('click', function (evt) {
+      evt.preventDefault();
+
+      var blockId = anchor.getAttribute('href');
+      document.querySelector('' + blockId).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    });
+  });
+})();
+
+(function () {
+  inputUserPhone.addEventListener('input', function () {
+    inputUserPhone.setCustomValidity('');
+  });
+
+  inputUserPhone.addEventListener('invalid', function () {
+    if (inputUserPhone.validity.valueMissing) {
+      inputUserPhone.setCustomValidity('Обязательное поле');
+      return;
+    }
+    if (inputUserPhone.validity.patternMismatch) {
+      inputUserPhone.setCustomValidity('Вводите только числа');
+      return;
+    }
+    inputUserPhone.setCustomValidity('');
+  });
+})();
+
+(function () {
+  var resetForm = function () {
+    var inputUserName = form.querySelector('#user-name');
+    inputUserName.value = '';
+    inputUserPhone.value = '';
+  };
+
+  form.addEventListener('submit', function (evt) {
     evt.preventDefault();
 
-    var blockId = anchor.getAttribute('href');
-    document.querySelector('' + blockId).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    var formData = new FormData(evt.target);
+
+    fetchData('https://echo.htmlacademy.ru/',
+        {
+          method: 'POST',
+          body: formData,
+        }, function () {
+          resetForm();
+        });
   });
-});
 
-inputUserPhone.addEventListener('input', function () {
-  inputUserPhone.setCustomValidity('');
-});
-
-inputUserPhone.addEventListener('invalid', function () {
-  if (inputUserPhone.validity.valueMissing) {
-    inputUserPhone.setCustomValidity('Обязательное поле');
-    return;
-  }
-  if (inputUserPhone.validity.patternMismatch) {
-    inputUserPhone.setCustomValidity('Вводите только числа');
-    return;
-  }
-  inputUserPhone.setCustomValidity('');
-});
-
-var resetForm = function () {
-  var inputUserName = form.querySelector('#user-name');
-  inputUserName.value = '';
-  inputUserPhone.value = '';
-};
-
-form.addEventListener('submit', function (evt) {
-  evt.preventDefault();
-
-  var formData = new FormData(evt.target);
-
-  fetchData('https://echo.htmlacademy.ru/',
-      {
-        method: 'POST',
-        body: formData,
-      }, function () {
-        resetForm();
+  var fetchData = function (url, options, onSuccess) {
+    fetch(url, options)
+      .then(function (response) {
+        response.json();
+      })
+      .then(function (response) {
+        onSuccess(response);
       });
-});
-
-var fetchData = function (url, options, onSuccess) {
-  fetch(url, options)
-    .then(function (response) {
-      response.json();
-    })
-    .then(function (response) {
-      onSuccess(response);
-    });
-};
+  };
+})();
